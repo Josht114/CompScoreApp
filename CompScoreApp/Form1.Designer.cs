8de601369919fb57f1d960d4dad88fda7e9e2301
Josh <josht114@hotmail.com>
2023-03-30 20:28:52 +0100

    Added timer

--- CompScoreApp/Form1.Designer.cs
+++ CompScoreApp/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            timeLabel = new Label();
+            startStop = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ticker = new Label();
+            secondslabel = new Label();
+            SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            timeLabel.AutoSize = true;
+            timeLabel.Location = new Point(347, 61);
+            timeLabel.Name = "timeLabel";
+            timeLabel.Size = new Size(40, 20);
+            timeLabel.TabIndex = 0;
+            timeLabel.Text = "xx:xx";
+            // 
+            // startStop
+            // 
+            startStop.Location = new Point(338, 111);
+            startStop.Name = "startStop";
+            startStop.Size = new Size(94, 29);
+            startStop.TabIndex = 1;
+            startStop.Text = "Start / Stop";
+            startStop.UseVisualStyleBackColor = true;
+            startStop.Click += startStop_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // ticker
+            // 
+            ticker.AutoSize = true;
+            ticker.Location = new Point(616, 41);
+            ticker.Name = "ticker";
+            ticker.Size = new Size(17, 20);
+            ticker.TabIndex = 2;
+            ticker.Text = "0";
+            // 
+            // secondslabel
+            // 
+            secondslabel.AutoSize = true;
+            secondslabel.Location = new Point(653, 125);
+            secondslabel.Name = "secondslabel";
+            secondslabel.Size = new Size(50, 20);
+            secondslabel.TabIndex = 3;
+            secondslabel.Text = "label1";
+            secondslabel.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(secondslabel);
+            Controls.Add(ticker);
+            Controls.Add(startStop);
+            Controls.Add(timeLabel);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label timeLabel;
+        private Button startStop;
+        private System.Windows.Forms.Timer timer1;
+        private Label ticker;
+        private Label secondslabel;
     }
 }
--- CompScoreApp/Form1.cs
+++ CompScoreApp/Form1.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace CompScoreApp
 {
     public partial class Form1 : Form
@@ -5,6 +7,55 @@ namespace CompScoreApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int seconds = 100;
+        int tickerCount = 1;
+        bool timerRunning = false;
+
+
+
+
+
+        private void startStop_Click(object sender, EventArgs e)
+        {
+
+            if (timerRunning)
+            {
+                timer1.Stop();
+                timerRunning = false;
+            }
+            else
+            {
+                timer1.Start();
+                timerRunning = true;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan time = TimeSpan.FromSeconds(seconds);
+            string text = time.ToString(@"mm\.ss");
+
+            timeLabel.Text = text;
+            seconds = seconds -1;
+
+  
+
+            ticker.Text = tickerCount++.ToString();
+            secondslabel.Text = seconds.ToString();
+
+
+            if (seconds < 0)
+            {
+                 timer1.Stop();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
